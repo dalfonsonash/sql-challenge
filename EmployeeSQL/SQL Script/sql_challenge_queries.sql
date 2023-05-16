1276690511b1c951cd5d0bbab38161d07a7d00ab
dalfonsonash <dalfonsonash@outlook.com>
2023-05-16 09:43:07 -0700

    Corrections to schema keys, and queries.

--- EmployeeSQL/SQL Script/sql_challenge_queries.sql
+++ EmployeeSQL/SQL Script/sql_challenge_queries.sql
@@ -52,16 +52,15 @@ WHERE d.dept_name = 'Sales';
 --7. List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
-FROM employees as e
-JOIN dept_emp as de
-ON e.emp_no = de.emp_no
-JOIN departments as d
-ON de.dept_no = d.dept_no
-WHERE d.dept_name = 'Sales' or d.dept_name = 'Development';
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name IN ('Sales', 'Development');
+
 
 --8. List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
 
-SELECT last_name, COUNT(last_name) as last_name_frequency
+SELECT last_name, COUNT(*) AS frequency
 FROM employees
 GROUP BY last_name
-ORDER BY last_name_frequency DESC;
+ORDER BY frequency DESC;
--- EmployeeSQL/SQL Script/table_schemas.sql
+++ EmployeeSQL/SQL Script/table_schemas.sql
@@ -16,14 +16,15 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE dept_emp (
-  emp_no  INT PRIMARY KEY,
+  emp_no  INT,
   dept_no VARCHAR(10),
+  PRIMARY KEY (emp_no, dept_no),
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
 
 CREATE TABLE dept_manager (
-  dept_no VARCHAR(6),
+  dept_no VARCHAR(10),
   emp_no  INT PRIMARY KEY,
   FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no)

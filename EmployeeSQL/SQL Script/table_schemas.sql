0473b8be470a9b0143bb17013a11ae74b285088c
D. Alfonso-Nash <126922261+dalfonsonash@users.noreply.github.com>
2023-05-16 08:45:56 -0700

    Update table_schemas.sql

--- EmployeeSQL/SQL Script/table_schemas.sql
+++ EmployeeSQL/SQL Script/table_schemas.sql
@@ -5,6 +5,16 @@ CREATE TABLE departments (
   dept_name VARCHAR(25) NOT NULL
 );
 
+CREATE TABLE employees (
+  emp_no        INT PRIMARY KEY,
+  emp_title_id  VARCHAR(10),
+  birth_date    DATE,
+  first_name    VARCHAR(255),
+  last_name     VARCHAR(255),
+  sex           CHAR(1),
+  hire_date     DATE
+);
+
 CREATE TABLE dept_emp (
   emp_no  INT PRIMARY KEY,
   dept_no VARCHAR(10),
@@ -13,25 +23,11 @@ CREATE TABLE dept_emp (
 );
 
 CREATE TABLE dept_manager (
-  dept_no VARCHAR(4),
+  dept_no VARCHAR(6),
   emp_no  INT PRIMARY KEY,
   FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
-
-CREATE TABLE employees (
-  emp_no        INT PRIMARY KEY,
-  emp_title_id  VARCHAR(5),
-  birth_date    DATE,
-  first_name    VARCHAR(255),
-  last_name     VARCHAR(255),
-  sex           CHAR(1),
-  hire_date     DATE
-);
-
-ALTER TABLE employees
-ALTER COLUMN first_name TYPE VARCHAR(25),
-ALTER COLUMN last_name TYPE VARCHAR(25); 
 
 CREATE TABLE salaries (
   emp_no INT,
@@ -40,10 +36,8 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE titles (
-  title_id VARCHAR(4) PRIMARY KEY,
+  title_id VARCHAR(10) PRIMARY KEY,
   title    VARCHAR(25) NOT NULL
 );
 
-ALTER TABLE titles
-ALTER COLUMN title_id TYPE VARCHAR(10);
 

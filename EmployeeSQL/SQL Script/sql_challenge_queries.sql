94e1bcb7f768c395246882dc7af589f9a2c43330
dalfonsonash <dalfonsonash@outlook.com>
2023-05-16 08:59:35 -0700

    Correction to query syntax.

--- EmployeeSQL/SQL Script/sql_challenge_queries.sql
+++ EmployeeSQL/SQL Script/sql_challenge_queries.sql
@@ -4,9 +4,9 @@
 -- 1.List the employee number, last name, first name, sex, and salary of each employee.
  
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
-	FROM employees as e
-	JOIN salaries as s
-		ON e.emp_no = s.emp_no;
+FROM employees as e
+JOIN salaries as s
+ON e.emp_no = s.emp_no;
 
 -- 2. List the first name, last name, and hire date for the employees who were hired in 1986.
 
